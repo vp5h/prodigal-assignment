30f07eeff261d7260ab4ddab2937fc8ce84ed55f
Pravesh Vyas <praveshvyas.18@gmail.com>
2022-01-04 17:35:09 +0530

    try

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -13,7 +13,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/dist',
+      publicPath: '/',
     },
     options.output,
   ), // Merge with env dependent settings
